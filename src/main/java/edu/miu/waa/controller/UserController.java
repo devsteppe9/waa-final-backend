6a55e2205fffc62eb044d39b109b067b0dd5f912
Viet Nguyen <viet@dhis2.org>
2025-03-17 16:15:47 -0500

    feat: project intitial commit

---
+++ src/main/java/edu/miu/waa/controller/UserController.java
@@ -0,0 +1,15 @@
+package edu.miu.waa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+  
+  @GetMapping
+  public String getAllUsers() {
+    return "All users";
+  }
+}
