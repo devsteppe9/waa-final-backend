6547a4326025fa0c0accf15b667b99886a675a2b
Viet Nguyen <viet@dhis2.org>
2025-03-18 17:41:39 -0500

    fix: add unit-test profile to github action

--- src/test/java/edu/miu/waa/controller/AbstractControllerTest.java
+++ src/test/java/edu/miu/waa/controller/AbstractControllerTest.java
@@ -2,15 +2,13 @@ package edu.miu.waa.controller;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@TestPropertySource(locations = "classpath:application-test.properties")
-@Profile("unit-test")
-@ActiveProfiles("unit-test")
+@TestPropertySource(locations = "classpath:application-unit-test.properties")
+@ActiveProfiles({"unit-test"})
 @Transactional
 public class AbstractControllerTest {}
--- src/test/java/edu/miu/waa/repo/AbstractRepoTest.java
+++ src/test/java/edu/miu/waa/repo/AbstractRepoTest.java
@@ -2,7 +2,6 @@ package edu.miu.waa.repo;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,8 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@TestPropertySource(locations = "classpath:application-test.properties")
-@Profile("unit-test")
-@ActiveProfiles("unit-test")
+@TestPropertySource(locations = "classpath:application-unit-test.properties")
+@ActiveProfiles({"unit-test"})
 @Transactional
 public class AbstractRepoTest {}
