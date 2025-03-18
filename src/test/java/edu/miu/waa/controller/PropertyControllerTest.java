a779caf44870686035a895ac5f5e89ba55427135
Viet Nguyen <viet@dhis2.org>
2025-03-17 22:17:39 -0500

    feat: add PropertyControllerTest

--- src/test/java/edu/miu/waa/controller/PropertyControllerTest.java
+++ src/test/java/edu/miu/waa/controller/PropertyControllerTest.java
@@ -11,29 +11,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
 public class PropertyControllerTest {
+  
   @Autowired private MockMvc mockMvc;
+  
   private ObjectMapper objectMapper = new ObjectMapper();
+  
   @Autowired
   private PropertyService propertyService;
-  
+
   @Test
+  @WithMockUser(value = "test")
   void testFindAll() throws Exception {
     Property p = new Property();
     p.setName("test");
     propertyService.create(p);
 
     MvcResult result = mockMvc.perform(get("/api/v1/properties")).andExpect(status().isOk()).andReturn();
-    Property[] props = objectMapper.readValue(result.getResponse().getContentAsString(),
+    Property[] properties = objectMapper.readValue(result.getResponse().getContentAsString(),
         Property[].class);
-    assertEquals(1, props.length);
+    assertEquals(1, properties.length);
   }
   
 }
