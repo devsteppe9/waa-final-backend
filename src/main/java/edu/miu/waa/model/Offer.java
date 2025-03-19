d8e9d0bbe5b087542e2785acc9bfa60d10c53b12
Jarkynbyek Japar <18748558+devsteppe9@users.noreply.github.com>
2025-03-19 16:44:07 -0500

    feat: add Offer and OfferStatusEnum models with relationships to Property and User

--- src/main/java/edu/miu/waa/model/Offer.java
+++ src/main/java/edu/miu/waa/model/Offer.java
@@ -1,17 +1,31 @@
 package edu.miu.waa.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
 @Entity
 public class Offer {
-
+    // Product Name, product price, product image, offer status, created date
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private OfferStatusEnum status;
+    private LocalDate createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
