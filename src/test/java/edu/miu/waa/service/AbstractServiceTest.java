b177e619351a54a089b6626188e35f06a0a4901e
Viet Nguyen <viet@dhis2.org>
2025-03-21 11:40:49 -0500

    fix service test

--- src/test/java/edu/miu/waa/service/AbstractServiceTest.java
+++ src/test/java/edu/miu/waa/service/AbstractServiceTest.java
@@ -1,13 +1,17 @@
 package edu.miu.waa.service;
 
+import edu.miu.waa.config.TestSecurityConfig;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Import(TestSecurityConfig.class)
 @TestPropertySource(locations = "classpath:application-unit-test.properties")
-@ActiveProfiles("unit-test")
 @Transactional
+@ActiveProfiles("unit-test")
 public class AbstractServiceTest {}
--- src/test/java/edu/miu/waa/util/JwtUtilTest.java
+++ src/test/java/edu/miu/waa/util/JwtUtilTest.java
@@ -14,7 +14,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 
-@Disabled
 class JwtUtilTest extends AbstractServiceTest {
 
     @Autowired
